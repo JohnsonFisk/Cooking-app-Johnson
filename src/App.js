0fba00a2f39e8253f0bd12cb5e2eb6ab8d32585b
Johnson <mj1929@hotmail.fr>
2024-05-21 19:06:17 +0200

    fetcht de l'API recette

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const App = () => {
+
+    useEffect(() => {
+        axios
+        .get("https://www.themealdb.com/api/json/v1/1/filter.php?i=").then((res) => console.log(res.data))
+    })
+
+
+
   return (
     <div>
-      
+    <div className="app-container">
+        <nav>
+            <h2>Cooking app Johnson</h2>
+            <input type="text" placeholder="Tapez un nom d'aliment (en anglais)"/>
+        </nav>
+        
+        <div className="card-container">
+
+        </div>
+    </div>
     </div>
   );
 };
